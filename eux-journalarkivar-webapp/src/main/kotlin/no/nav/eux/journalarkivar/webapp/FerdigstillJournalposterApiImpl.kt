b49d486dc268b0674be12a063eb0a77de471b951
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-04-18 14:57:16 +0200

    unprotected

--- eux-journalarkivar-webapp/src/main/kotlin/no/nav/eux/journalarkivar/webapp/FerdigstillJournalposterApiImpl.kt
+++ eux-journalarkivar-webapp/src/main/kotlin/no/nav/eux/journalarkivar/webapp/FerdigstillJournalposterApiImpl.kt
@@ -2,7 +2,7 @@ package no.nav.eux.journalarkivar.webapp
 
 import no.nav.eux.journal.openapi.api.FerdigstillJournalposterApi
 import no.nav.eux.journalarkivar.service.JournalService
-import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +12,7 @@ class FerdigstillJournalposterApiImpl(
     val journalService: JournalService
 ) : FerdigstillJournalposterApi {
 
-    @Protected
+    @Unprotected
     override fun ferdigstillJournalposter(body: Any): ResponseEntity<Unit> {
         journalService.ferdigstillJournalposter()
         return ResponseEntity(NO_CONTENT)
