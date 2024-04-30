1ca0b94249f20c9c617b651418547dec970ce70d
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-04-30 14:09:40 +0200

    arkivarprosess

--- eux-journalarkivar-webapp/src/main/kotlin/no/nav/eux/journalarkivar/webapp/ArkivarprosessApiImpl.kt
+++ eux-journalarkivar-webapp/src/main/kotlin/no/nav/eux/journalarkivar/webapp/ArkivarprosessApiImpl.kt
@@ -2,6 +2,7 @@ package no.nav.eux.journalarkivar.webapp
 
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import no.nav.eux.journal.openapi.api.ArkivarprosessApi
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,7 @@ class ArkivarprosessApiImpl : ArkivarprosessApi {
 
     val log = logger {}
 
+    @Unprotected
     override fun arkivarprosess(arkivarprosess: String): ResponseEntity<Unit> {
         log.info { "Arkivarprosess: $arkivarprosess" }
         return ResponseEntity(HttpStatus.NO_CONTENT)
